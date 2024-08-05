47a369cbe8e77acdadee8078822747ab8b1e5e35
Hemanshu Varma <hemanshuvarma916@gmail.com>
2024-08-05 22:39:27 +0530

    Add: Appbar and battery container

--- app/src/main/java/org/foundation/tara/ibattery/MainActivity.kt
+++ app/src/main/java/org/foundation/tara/ibattery/MainActivity.kt
@@ -5,14 +5,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.foundation.tara.ibattery.receivers.SystemBroadcastReceiver
 import org.foundation.tara.ibattery.ui.screens.BatteryInfoScreen
 import org.foundation.tara.ibattery.ui.theme.IBatteryTheme
@@ -52,7 +58,23 @@ fun BatteryInfoApp(modifier: Modifier = Modifier) {
             viewModel.updateBatteryChargeStatus(receiveIntent)
         }
     }
-    BatteryInfoScreen(modifier = modifier, percentBattery, chargeStatus)
+
+    Scaffold(topBar = {
+        Appbar()
+    }) { paddingValues ->
+        BatteryInfoScreen(modifier = modifier.padding(paddingValues), percentBattery, chargeStatus)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Appbar() {
+    CenterAlignedTopAppBar(title = {
+        Text(
+            text = stringResource(id = R.string.app_name),
+            style = MaterialTheme.typography.headlineMedium
+        )
+    })
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/org/foundation/tara/ibattery/ui/screens/BatteryInfo.kt
+++ app/src/main/java/org/foundation/tara/ibattery/ui/screens/BatteryInfo.kt
@@ -2,17 +2,20 @@ package org.foundation.tara.ibattery.ui.screens
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.foundation.tara.ibattery.ui.anims.WavesIndicator
@@ -33,14 +36,25 @@ fun BatteryInfoScreen(modifier: Modifier, batteryPercent: Float, isCharging: Boo
                 .fillMaxWidth(0.5f)
                 .fillMaxHeight(0.5f),
             border = BorderStroke(
-                4.dp, Color.Cyan
+                3.dp, Color.Gray
             )
         ) {
-            WavesIndicator(
-                modifier = Modifier.fillMaxSize().padding(8.dp),
-                color = Color.Green,
-                progress = batteryPercent / 100f
-            )
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                WavesIndicator(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Green,
+                    progress = batteryPercent / 100f
+                )
+                Text(
+                    text = batteryPercent.toInt().toString(),
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.headlineLarge
+                )
+            }
+
         }
         Text(
             modifier = Modifier.padding(8.dp),
@@ -56,7 +70,7 @@ fun BatteryInfoScreenPreview() {
     IBatteryTheme {
         BatteryInfoScreen(
             modifier = Modifier,
-            batteryPercent = 56f,
+            batteryPercent = 80f,
             isCharging = false
         )
     }
